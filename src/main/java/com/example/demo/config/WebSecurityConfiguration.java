5920a85933eae36f98b2458564b347231e970b43
Skander Ayachi <ayachiskander44@mail.com>
2025-09-22 23:48:05 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo.config;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+
+    
+     // Configuration CORS pour le front-end
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "https://gmao-skander.netlify.app",
+                "http://localhost:4200",
+                "http://127.0.0.1:4200",
+                "https://app.netlify.com"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+
+    }
+}
