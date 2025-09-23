a26bd52387e01a4de20236d2387a9d6315d173af
Skander Ayachi <ayachiskander44@mail.com>
2025-09-23 23:23:19 +0200

    Mise à jour de WebSecurityConfiguration (échange d'image ou modification)

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")   // toutes les routes
+                .allowedOrigins(
+                    "https://gmao-app.netlify.app",  // ton front sur Netlify
+                    "http://localhost:4200"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
